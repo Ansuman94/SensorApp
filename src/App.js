2b669c1ef3b584568bf0112c9b8a60aa889c6c54
Ansuman Mohapatra <ansumanmohapatra94@gmail.com>
2019-09-28 11:23:13 +0530

    'initialfolderstructure'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./View/View";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <View />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/View/View.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./View.css";
+
+function View() {
+  return <div className="view">View</div>;
+}
+
+export default View;
